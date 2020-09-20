04ff9f0c9fd023a3900ee3bd570bda7e87d65863
Jianing <jianingw321@outlook.com>
2020-09-20 12:44:45 +0800

    first commit

---
+++ src/components/app/HelloWorld.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./HelloWorld.css";
+
+class HelloWorld extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className="font-color">第一个React页面</h1>
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+}
+
+export default HelloWorld;
